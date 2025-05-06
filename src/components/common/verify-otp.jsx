bcc05aae74cb533719b62f6d347fd69e228fd9ae
barozzxb <work.xuanbao@gmail.com>
2025-05-06 18:33:56 +0700

    Update verify-otp.jsx

--- src/components/common/verify-otp.jsx
+++ src/components/common/verify-otp.jsx
@@ -131,7 +131,7 @@ export default function VerifyOTP() {
   // Gửi OTP ngay khi có email
   useEffect(() => {
     if (email) sendCode(email);
-  }, [emaile]);
+  }, [email]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
