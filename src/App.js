13959922f36da12f4058d1fb828d3f2516ad1a2e
Ngọc Vân <nguyenngocvan.qng@gmail.com>
2025-05-13 05:45:10 +0700

    Resolved merge conflicts between main and ngocvan

--- src/App.js
+++ src/App.js
@@ -23,27 +23,9 @@ import Employer from "./components/employer/employer.jsx";
 import VerifyOTP from "./components/common/verify-otp.jsx";
 import ApplicantProfile from "./components/applicant/profile.jsx";
 import ForgotPassword from "./components/common/forgotpassword.jsx";
-
-<<<<<<< HEAD
 import JobDetail from "./components/common/jobdetail.jsx";
 import JobList from "./components/common/listjobs.jsx";
-
 import "./components/assets/css/styles.css";
-=======
-import NavBar from './components/common/Nav-bar.jsx';
-import Footer from './components/common/Footer.jsx';
-import Homepage from './components/common/Homepage.jsx';
-import Login from './components/common/Login.jsx';
-import Register from './components/common/Register.jsx';
-import Applicant from './components/applicant/Applicant.jsx';
-import Employer from './components/employer/employer.jsx';
-import VerifyOTP from './components/common/Verify-otp.jsx';
-import ApplicantProfile from './components/applicant/Profile.jsx';
-import ForgotPassword from './components/common/Forgotpassword.jsx';
-
-import JobDetail from './components/common/Jobdetail.jsx';
-import JobList from './components/common/Listjobs.jsx';
->>>>>>> 0e1294f00099bc8d2672253e48f2becea143556d
 
 // import PostJob from "./components/employer/PostJob.jsx";
 import ManageJobs from "./components/employer/ManageJobs.jsx";
@@ -61,18 +43,7 @@ import ViewReports from "./components/admin/ViewReports.jsx";
 
 import EmployerNavBar from "./components/employer/EmployerNavbar.jsx";
 
-<<<<<<< HEAD
 import SupportDetail from "./components/admin/SupportDetail.jsx";
-=======
-import AdminLayout from './components/admin/AdminLayout.jsx';
-import Dashboard from './components/admin/Dashboard.jsx';
-import SendEmail from './components/admin/SendEmail.jsx';
-import ManageAccounts from './components/admin/ManageAccounts.jsx';
-import SupportUser from './components/admin/SupportUser.jsx';
-import ViewReports from './components/admin/ViewReports.jsx';
-import AdminProfile from "./components/admin/AdminProfile.jsx"
-import AdminNotificationsPage from "./components/admin/AdminNotificationsPage.jsx"
->>>>>>> 0e1294f00099bc8d2672253e48f2becea143556d
 
 import NotFound from "./components/error/404.jsx";
 import Error403 from "./components/error/403.jsx";
@@ -89,26 +60,8 @@ function AppContent() {
 
   return (
     <>
-<<<<<<< HEAD
       {/* Navbar theo role */}
       {!isAdminRoute && (isEmployerRoute ? <EmployerNavBar /> : <NavBar />)}
-=======
-      {/* //{!isAdminRoute && <EmployerNavbar />} */}
-      {!isAdminRoute && (
-        location.pathname.startsWith('/employer') ? <EmployerNavBar /> : <NavBar />
-      )}
-      
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={true}
-        closeOnClick
-        rtl={false}
-        pauseOnHover
-        draggable
-      />
->>>>>>> 0e1294f00099bc8d2672253e48f2becea143556d
 
       <ToastContainer position="top-right" autoClose={5000} pauseOnHover />
 
@@ -123,17 +76,9 @@ function AppContent() {
           <Route path="/jobs/:id" element={<JobDetail />} />
           <Route path="/jobs" element={<JobList />} />
 
-<<<<<<< HEAD
           {/* APPLICANT */}
           <Route element={<ProtectedRoute allowedRoles={["APPLICANT"]} />}>
             <Route path="/applicant" element={<Employee />} />
-=======
-
-
-          {/* Các Route dành cho ứng viên */}
-          <Route element={<ProtectedRoute allowedRoles={['APPLICANT']} />}>
-            <Route path="/applicant" element={<Applicant />} />
->>>>>>> 0e1294f00099bc8d2672253e48f2becea143556d
             <Route path="/applicant/profile" element={<ApplicantProfile />} />
             <Route
               path="/applicant/manage-saved-jobs"
@@ -164,16 +109,10 @@ function AppContent() {
               <Route path="manage-accounts" element={<ManageAccounts />} />
               <Route path="support-user" element={<SupportUser />} />
               <Route path="view-reports" element={<ViewReports />} />
-<<<<<<< HEAD
               <Route
                 path="support-user/support/:id"
                 element={<SupportDetail />}
               />
-=======
-              <Route path="support-user/support/:id" element={<SupportDetail />} />
-              <Route path="profile" element={<AdminProfile />} />
-              <Route path="notifications" element={<AdminNotificationsPage />} />
->>>>>>> 0e1294f00099bc8d2672253e48f2becea143556d
             </Route>
           </Route>
 
